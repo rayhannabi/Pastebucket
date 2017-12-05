7761a52749ad1b78dd5c02f3d7d3efaca5ba3703
Rayhan Janam <rayhanjanam@gmail.com>
2017-12-06 04:05:06 +0600

    implemented UISearchControler

--- PasteBucket/Controller/PastesTVC.swift
+++ PasteBucket/Controller/PastesTVC.swift
@@ -12,6 +12,9 @@ class PastesTVC: UITableViewController {
 
     var demoData: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
+    // Searchbar
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,17 +23,27 @@ class PastesTVC: UITableViewController {
         editButton.tintColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         
         self.navigationItem.leftBarButtonItem = editButton
+        
+        // search controller init
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search your Pastes"
+        searchController.searchBar.tintColor = UIColor.white
+        
+        print(searchController.searchBar.value(forKey: "searchField")!)
+        
+        definesPresentationContext = true
+        self.navigationItem.searchController = self.searchController
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = [NSAttributedStringKey.foregroundColor.rawValue: UIColor.white]
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return demoData.count
     }
 
@@ -72,29 +85,11 @@ class PastesTVC: UITableViewController {
         return [deleteAction, shareAction]
     }
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+}
 
+extension PastesTVC: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        // update
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
